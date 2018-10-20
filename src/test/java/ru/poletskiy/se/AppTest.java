b9e36afbda43fa3e10d8245ec0ca2f83c2f80fa8
Poletskiy Maxim <mpoletsky@mail.ru>
2018-10-20 15:37:42 +0600

    Создал новый проект

---
+++ src/test/java/ru/poletskiy/se/AppTest.java
@@ -0,0 +1,20 @@
+package ru.poletskiy.se;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
